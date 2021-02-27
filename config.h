3861f2f18ffe11a42b8bfda2febb3c20462b1886
Adam Podkowinski <adampodkdev@gmail.com>
2021-02-27 14:29:01 +0100

    its hard1

--- config.h
+++ config.h
@@ -6,10 +6,7 @@ static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
 static const int gappx = 14;
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;     /* 0 means no systray */
+
 static const char *fonts[] = {"FiraCode Nerd Font:size=10",
                               "Noto Color Emoji:size=10"};
 
