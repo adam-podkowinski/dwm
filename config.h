d4d8c2c3527542e344fd3054a89731fd4720964d
adampodkdev@gmail.com <adampodkdev@gmail.com>
2022-01-09 18:09:23 +0100

    Make st not swallow

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static const Rule rules[] = {
     {"Mailspring",     NULL,       NULL,       1 << 7,       0,            0,           0,           -1},
     {"Pavucontrol", NULL,     NULL,       0,            1,            0,           0,           -1},
     {"Spotify",   NULL,       NULL,       1 << 4,       0,            0,           0,           -1},
-    {"St",        NULL,       NULL,       0,            0,            1,           0,           -1},
+    {"St",        NULL,       NULL,       0,            0,            0,           0,           -1},
     {NULL,        NULL,       "Event Tester", 0,        0,            0,           1,           -1}, /* xev */
 
 };
